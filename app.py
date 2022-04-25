d5926f55f23b376a5d72973537a976670b7df51d
Samuel Dion <samuelqwe@icloud.com>
2022-04-25 15:44:20 -0400

    Implement basic user authentication

--- app.py
+++ app.py
@@ -1,29 +1,80 @@
-import os
-import psycopg2
-from flask import Flask, render_template
+from functools import wraps
+import psycopg2.extras
+from flask import Flask, flash, redirect, render_template, url_for, abort, request, g
 
 app = Flask(__name__)
+app.secret_key = "secretKey"
 app.config.from_object("config.Config")
 
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=app.config["DB_HOST"],
-        port=app.config["DB_PORT"],
-        database=app.config["DB_NAME"],
-        user=app.config["DB_USERNAME"],
-        password=app.config["DB_PASSWORD"])
-    return conn
+loggedInUser = None
 
+def login_required(user_type = None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not loggedInUser:
+                return redirect(url_for('login'))
+            elif user_type and not loggedInUser['role'] == user_type:
+                return abort(403)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def get_db_connection():
+    if 'db_conn' not in g:
+        g.db_conn = psycopg2.connect(
+            host=app.config["DB_HOST"],
+            port=app.config["DB_PORT"],
+            database=app.config["DB_NAME"],
+            user=app.config["DB_USERNAME"],
+            password=app.config["DB_PASSWORD"])
+        
+    return g.db_conn
+
+# Automatically manage the database connection, as necessary
+@app.teardown_appcontext
+def teardown_db(exception):
+    db_conn = g.pop('db_conn', None)
+    
+    if db_conn is not None:
+        db_conn.close()
 
 @app.route('/')
+@login_required()
 def index():
+    # TODO: Redirect to proper user location based on role
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM books;')
-    books = cur.fetchall()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT * FROM utilisateurs;')
+    users = cur.fetchall()
     cur.close()
-    conn.close()
-    return render_template('index.html', books=books)
+    return render_template('index.html', users=users)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("SELECT * FROM utilisateurs WHERE username='{}' AND password='{}';".format(username, password))
+        user = cur.fetchone()
+        cur.close()
+        
+        if user:
+            global loggedInUser
+            loggedInUser = user
+            return redirect(url_for('index'))
+        else:
+            flash("L'utilisateur n'a pas été trouvé. Assurez-vous d'avoir bien entré les détails de connection.")
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    global loggedInUser
+    loggedInUser = None
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(port=7832, debug=True)
--- init_db.py
+++ init_db.py
@@ -1,4 +1,3 @@
-import os
 import psycopg2
 import config as appConfig
 
@@ -13,33 +12,21 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute('DROP TABLE IF EXISTS books;')
-cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                'title varchar (150) NOT NULL,'
-                                'author varchar (50) NOT NULL,'
-                                'pages_num integer NOT NULL,'
-                                'review text,'
-                                'date_added date DEFAULT CURRENT_TIMESTAMP);'
-                                )
+cur.execute('DROP TABLE IF EXISTS utilisateurs;')
+cur.execute('CREATE TABLE utilisateurs (id serial PRIMARY KEY,'
+                'username varchar (250) NOT NULL,'
+                'password varchar (250) NOT NULL,'
+                'role varchar(10) NOT NULL);'
+                )
 
 # Insert data into the table
 
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-        'VALUES (%s, %s, %s, %s)',
-        ('A Tale of Two Cities',
-        'Charles Dickens',
-        489,
-        'A great classic!')
-        )
-
-
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
-        'VALUES (%s, %s, %s, %s)',
-        ('Anna Karenina',
-        'Leo Tolstoy',
-        864,
-        'Another great classic!')
-        )
+cur.execute('INSERT INTO utilisateurs (username, password, role)'
+                'VALUES (%s, %s, %s)',
+                ('testuser',
+                'password',
+                'admin')
+                )
 
 conn.commit()
 
