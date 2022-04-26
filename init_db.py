c799e75be604cbe5cc95fb5680ab30bd2e7c1af3
Samuel Dion <samuelqwe@icloud.com>
2022-04-25 20:42:31 -0400

    Make usernames unique and add patients table

--- init_db.py
+++ init_db.py
@@ -11,21 +11,30 @@ conn = psycopg2.connect(
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-# Execute a command: this creates a new table
+# Create tables and fill them with default values
 cur.execute('DROP TABLE IF EXISTS utilisateurs;')
-cur.execute('CREATE TABLE utilisateurs (id serial PRIMARY KEY,'
-                'username varchar (250) NOT NULL,'
-                'password varchar (250) NOT NULL,'
+cur.execute('CREATE TABLE utilisateurs (id serial PRIMARY KEY UNIQUE,'
+                'username varchar NOT NULL UNIQUE,'
+                'password varchar NOT NULL,'
                 'role varchar(10) NOT NULL);'
                 )
-
-# Insert data into the table
 
 cur.execute('INSERT INTO utilisateurs (username, password, role)'
                 'VALUES (%s, %s, %s)',
                 ('testuser',
                 'password',
                 'admin')
+                )
+
+cur.execute('CREATE TABLE patients (patient_id serial PRIMARY KEY UNIQUE,'
+                'nom varchar NOT NULL,'
+                'sexe varchar NOT NULL,'
+                'assurance varchar,'
+                'SSN integer NOT NULL,'
+                'email varchar NOT NULL,'
+                'date_naissance date NOT NULL,'
+                'telephone integer NOT NULL,'
+                'address varchar(200) NOT NULL);'
                 )
 
 conn.commit()
